c65dc776f18e69d9dbcdd25af6133c1c603d91b9
Sankalp Swarup <80690415+sankalp20@users.noreply.github.com>
2023-04-23 08:54:27 +0530

    Time: 42 ms (89.66%), Space: 43.5 MB (71.26%) - LeetHub

---
+++ 1416-restore-the-array/1416-restore-the-array.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int numberOfArrays(String s, int k) {
+        int kMod = 1000000007;
+        int n = s.length();
+        int[] dp = new int[n];
+        if (s.charAt(n - 1) != '0') {
+            dp[n - 1] = 1;
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                continue;
+            }
+            long temp = 0;
+            int j = i;
+            while (j < n && temp * 10 + s.charAt(j) - '0' <= k) {
+                temp = temp * 10 + s.charAt(j) - '0';
+                if (j == n - 1) {
+                    dp[i] += 1;
+                } else {
+                    dp[i] += dp[j + 1];
+                }
+                dp[i] %= kMod;
+                j++;
+            }
+        }
+        return dp[0];
+    }
+}
