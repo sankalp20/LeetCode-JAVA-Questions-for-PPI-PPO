03601cadeb3297e2db935ae23e8f34f4d579fd3c
Sankalp Swarup <80690415+sankalp20@users.noreply.github.com>
2023-05-17 09:45:38 +0530

    Time: 0 ms (100.00%), Space: 58.9 MB (24.42%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int offset = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, offset - 1);
+        reverse(nums, offset, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
