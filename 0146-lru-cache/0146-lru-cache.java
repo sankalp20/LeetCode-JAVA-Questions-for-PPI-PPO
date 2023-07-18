0bd901b53874a4b7fcfd2eab7e941102d21e0773
Sankalp Swarup <80690415+sankalp20@users.noreply.github.com>
2023-07-18 06:24:58 +0530

    Time: 45 ms (99.39%), Space: 107.6 MB (99.29%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,57 +1,107 @@
-class LRUCache {  
-  Node head = new Node(0, 0), tail = new Node(0, 0);
-  Map<Integer, Node> map = new HashMap();
-  int capacity;
-  
-  public LRUCache(int _capacity) {
-    capacity = _capacity;
-    head.next = tail;
-    tail.prev = head;
-  }
+class LRUCache {
+    class Node{
+        int key;
+        int value;
 
-  public int get(int key) {
-    if(map.containsKey(key)) {
-      Node node = map.get(key);
-      remove(node);
-      insert(node);
-      return node.value;
-    } else {
-      return -1;
-    }
-  }
+        Node prev;
+        Node next;
 
-  public void put(int key, int value) {
-    if(map.containsKey(key)) {
-      remove(map.get(key));
+        Node(int key, int value){
+            this.key= key;
+            this.value= value;
+        }
     }
-    if(map.size() == capacity) {
-      remove(tail.prev);
+
+    public Node[] map;
+    public int count, capacity;
+    public Node head, tail;
+    
+    public LRUCache(int capacity) {
+        
+        this.capacity= capacity;
+        count= 0;
+        
+        map= new Node[10_000+1];
+        
+        head= new Node(0,0);
+        tail= new Node(0,0);
+        
+        head.next= tail;
+        tail.prev= head;
+        
+        head.prev= null;
+        tail.next= null;
     }
-    insert(new Node(key, value));
-  }
-  
-  private void remove(Node node) {
-    map.remove(node.key);
-    node.prev.next = node.next;
-    node.next.prev = node.prev;
-  }
-  
-  private void insert(Node node){
-    map.put(node.key, node);
-    node.next = head.next;
-    node.next.prev = node;
-    head.next = node;
-    node.prev = head;
-  }
-  
-  class Node{
-    Node prev, next;
-    int key, value;
-    Node(int _key, int _value) {
-      key = _key;
-      value = _value;
+    
+    public void deleteNode(Node node){
+        node.prev.next= node.next;
+        node.next.prev= node.prev;       
+        
+        return;
     }
-  }
+    
+    public void addToHead(Node node){
+        node.next= head.next;
+        node.next.prev= node;
+        node.prev= head;
+        
+        head.next= node;      
+        
+        return;
+    }
+    
+    public int get(int key) {
+        
+        if( map[key] != null ){
+            
+            Node node= map[key];
+            
+            int nodeVal= node.value;
+            
+            deleteNode(node);
+            
+            addToHead(node);
+            
+            return nodeVal;
+        }
+        else
+            return -1;
+    }
+    
+    public void put(int key, int value) {
+        
+        if(map[key] != null){
+            
+            Node node= map[key];
+            
+            node.value= value;
+            
+            deleteNode(node);
+            
+            addToHead(node);
+            
+        } else {
+            
+            Node node= new Node(key,value);
+            
+            map[key]= node;
+            
+            if(count < capacity){
+                count++;
+                addToHead(node);
+            } 
+            else {
+                
+                map[tail.prev.key]= null;
+                deleteNode(tail.prev);
+                
+                addToHead(node);
+            }
+        }
+        
+        return;
+    }
+    
 }
 
 /**
