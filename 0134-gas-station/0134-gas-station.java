ff111b00044c17be2692d56b809c728e76f8ded7
Sankalp Swarup <80690415+sankalp20@users.noreply.github.com>
2023-08-09 22:38:17 +0530

    Time: 3 ms (44.39%), Space: 54.1 MB (99.90%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int currGain = 0, totalGain = 0, ans = 0;
+        
+        for(int i = 0; i < gas.length; ++i){
+            totalGain += gas[i] - cost[i];
+            currGain += gas[i] - cost[i];
+            
+            if(currGain < 0){
+                ans = i + 1;
+                currGain = 0;
+            }
+        }
+        
+        return totalGain >= 0 ? ans : -1;
+    }
+}
