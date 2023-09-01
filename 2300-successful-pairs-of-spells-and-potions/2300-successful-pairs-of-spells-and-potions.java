b4b15b6aaff7f12485a26fe20216e4d90a1518da
Sankalp Swarup <80690415+sankalp20@users.noreply.github.com>
2023-09-01 11:31:22 +0530

    Time: 68 ms (35.48%), Space: 59.6 MB (10.81%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -1,43 +1,33 @@
 class Solution {
-    // Our implementation of lower bound method using binary search.
-    private int lowerBound(int[] arr, int key) {
-        int lo = 0;
-        int hi = arr.length;
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (arr[mid] < key) {
-                lo = mid + 1;
-            } else {
-                hi = mid;
-            }
-        }
-        return lo;
-    }
-    
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        // Sort the potions array in increasing order.
-        Arrays.sort(potions);
-        int[] answer = new int[spells.length];
-
+        int n = spells.length;
         int m = potions.length;
-        int maxPotion = potions[m - 1];
-
-        for (int i = 0; i < spells.length; i++) {
-            int spell = spells[i];
-            // Minimum value of potion whose product with current spell  
-            // will be at least success or more.
-            long minPotion = (long) Math.ceil((1.0 * success) / spell);
-            // Check if we don't have any potion which can be used.
-            if (minPotion > maxPotion) {
-                answer[i] = 0;
-                continue;
-            }
-            // We can use the found potion, and all potion in its right 
-            // (as the right potions are greater than the found potion).
-            int index = lowerBound(potions, (int) minPotion);
-            answer[i] = m - index;
+        
+        // Create an array of pairs containing spell and its original index
+        int[][] sortedSpells = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            sortedSpells[i][0] = spells[i];
+            sortedSpells[i][1] = i;
         }
+        
+        // Sort the 'spells with index' and 'potions' array in increasing order
+        Arrays.sort(sortedSpells, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(potions);
+        
+        // For each 'spell' find the respective 'minPotion' index
+        int[] answer = new int[n];
+        int potionIndex = m - 1;
 
+        for (int[] sortedSpell : sortedSpells) {
+            int spell = sortedSpell[0];
+            int index = sortedSpell[1];
+            
+            while (potionIndex >= 0 && (long) spell * potions[potionIndex] >= success) {
+                potionIndex -= 1;
+            }
+            answer[index] = m - (potionIndex + 1);
+        }
+        
         return answer;
     }
 }
